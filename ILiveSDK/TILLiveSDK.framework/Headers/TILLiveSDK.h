0ed45f730f360abb3621252c43eddf7863616f4b
ericxwli <ericxwli@ericxwli-mc0.tencent.com>
2018-06-16 18:52:22 +0800

    添加ILiveSDK代码

---
+++ ILiveSDK/TILLiveSDK.framework/Headers/TILLiveSDK.h
@@ -0,0 +1,17 @@
+//
+//  TILLiveSDK.h
+//  TILLiveSDK
+//
+//  Created by kennethmiao on 16/11/4.
+//  Copyright © 2016年 kennethmiao. All rights reserved.
+//
+
+#ifndef TILLiveSDK_h
+#define TILLiveSDK_h
+
+#import "TILLiveCommon.h"
+#import "TILLiveConfig.h"
+#import "TILLiveRoomOption.h"
+#import "TILLiveManager.h"
+
+#endif /* TILLiveSDK_h */
